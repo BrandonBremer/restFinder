c8fdea9e3e4343459af6ddeb468d937725cbd0af
Brandon Bremer <beb8nj@virginia.edu>
2021-05-24 11:45:57 -0400

    Last Commit Hopefully

--- restfinder/src/App.js
+++ restfinder/src/App.js
@@ -1,39 +1,122 @@
 import './App.css';
-import React, { useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Rest from './rest'
-export default function App() {
-  
-  //const [lat, setLat] = useState([]);
- // const [long, setLong] = useState([]);
-  const [data, setData] = useState([]);
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import data1 from "./data1.json";
 
+import mapboxgl from 'mapbox-gl';
+
+//import Filter from './input'
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmViOG5qIiwiYSI6ImNrb3locG90cTBtNGMyb3BncDVyOGQ0dXYifQ.jZZU_v_IQ5iQzWeBr_kbFA';
+export default function App() {
+  const[lat, setLat] = useState(38.0335540);
+  const [long, setLong] = useState(-78.5079800);
+  const [data, setData] = useState([data1]);
+  const [type, setType] = useState([]);
+ // const [data2,setData2] =useState([]);
+  const [locationData, setLocation] = useState([]);
+ 
+ // const mapContainer = useRef(null);
+ // const map = useRef(null);
+  const [ userInput, setUserInput ] = useState('');
+  const [ userInput2, setUserInput2 ] = useState('');
+  const [zoom, setZoom] = useState(12);
+  //setLat(lat = -33.8670522);
+  //setLong(long = 151.1957362);
   useEffect(() => {
-    const fetchData = async () => {
-      const url  = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?"); 
-      //navigator.geolocation.getCurrentPosition(function(position) {
-        //setLat(position.coords.latitude);
-        //setLong(position.coords.longitude);
-    //const location = `location=${lat},${long}`;
-    const location = 'location=-33.8670522,151.1957362';
+    const fetchData = async ()=> {
+     const url  = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?"); 
+
+  const location = `location=${lat},${long}`;
+   //const location = 'location=-33.8670522,151.1957362';
     const radius = '&radius=2000';
-    const type = '&keyword=restaurant';
+    const filter = `&keyword=${type}`;
     const key = '&key=AIzaSyCAaq8naO2mIatbIWG1U4tbVlrU36nE0YI';
 
-   // const restaurantSearchUrl = url + location + radius + type + key;
-   // await fetch(`${restaurantSearchUrl}`)
-   await fetch (`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=2000&keyword=restaurant&key=AIzaSyCAaq8naO2mIatbIWG1U4tbVlrU36nE0YI`)
+   const restaurantSearchUrl = url + location + radius + filter+ key;
+  
+   await fetch(`${restaurantSearchUrl}`)
+   //  await fetch ('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.0335540,-78.5079800&radius=2000&keyword=restaurant&key=AIzaSyCAaq8naO2mIatbIWG1U4tbVlrU36nE0YI')
       .then(res => res.json())
       .then(result => {
-        setData(result.results)
+   
+        setData(result)
+        
+     
       });
       
-    }
+    }   
     fetchData();
-  })
+   // if (map.current) return; // initialize map only once
+     //map.current = new mapboxgl.Map({
+    //container: mapContainer.current,
+    //style: 'mapbox://styles/mapbox/streets-v11',
+    //center: [long, lat],
+    //zoom: zoom
+ // });
 
+  })
+  
+ 
+  
+  
+    const handleChange = (e) => {
+        setUserInput(e.currentTarget.value)
+    }
+    const handleChange2 = (e) => {
+      setUserInput2(e.currentTarget.value)
+  }
+ 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        setType(userInput);
+      //  alert(data.results[0].name);
+        setUserInput("");
+    }
+ 
+    const handleSubmit2 = (e) => {
+      e.preventDefault();
+      let add = userInput2;
+      //{alert(lat)}
+      fetch (`http://api.positionstack.com/v1/forward?access_key=c36f5a82d94223eabe3f8a90ae3f6463&query=${add}`)
+      .then(res => res.json()) 
+      .then(result => {
+     
+       setLocation(result)
+   
+      });
+      if(typeof locationData.data != 'undefined')
+      {
+      setLat(locationData.data[0].latitude);
+      setLong(locationData.data[0].longitude);
+      }
+ 
+      setUserInput("");
+  }
+ //<div ref={mapContainer} className="map-container" />
   return (
     <div className="App">
-    <Rest restData={data}/>
+    
+      <form onSubmit={handleSubmit} >
+            <Input value={userInput} color="secondary" type="text" onChange={handleChange} placeholder="Enter what type of restaraunt you are looking for"/>
+            <Button variant="contained" color="primary" onClick={handleSubmit}>Filter</Button>
+            
+        </form>
+        <form onSubmit={handleSubmit2}>
+            <Input  color="secondary" value={userInput2} type="text" onChange={handleChange2} placeholder="What is your address?"/>
+            <Button variant="contained" color="primary" onClick={handleSubmit2}>Search by Location</Button>
+            
+        </form>
+       
+    {(typeof data.results != 'undefined')?
+    ( 
+     <Rest restData={data} />):
+     <div></div>
+    }
+      
+    
    </div>
   );
 }
--- restfinder/src/rest.js
+++ restfinder/src/rest.js
@@ -1,8 +1,388 @@
-import React from 'react';
+
+import React, { useRef, useEffect, useState } from "react";
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import mapboxgl from 'mapbox-gl';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+const Rest = ({restData}) => {
+//var dataSet = [];
+const mapContainer = useRef(null);
+const [map, setMap] = useState(null);
+//const map = useRef(null);
+const [data, setData] = useState(restData.results);
+const [zoom, setZoom] = useState(15);
+const[lat, setLat] = useState(38.0335540);
+const [long, setLong] = useState(-78.5079800);
+const styles = {
+  width: "100vw",
+  height: "calc(100vh - 80px)",
+  position: "absolute"
+};
+ // if (map.current) return; // initialize map only once
+ useEffect(() => {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYmViOG5qIiwiYSI6ImNrb3locG90cTBtNGMyb3BncDVyOGQ0dXYifQ.jZZU_v_IQ5iQzWeBr_kbFA';
+  const initializeMap = ({ setMap, mapContainer }) => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+      center: [restData.results[0].geometry.location.lng, restData.results[0].geometry.location.lat],
+      zoom: zoom
+    });
+
+    map.on("load", () => {
+      setMap(map);
+      map.resize();
+    });
+  };
+/*
+   if (!map) initializeMap({ setMap, mapContainer });
+ if(map){
+    var markerHeight = 50, markerRadius = 10, linearOffset = 25;
+
+    var popupOffsets = {
+      'top': [0, 0],
+      'top-left': [0,0],
+      'top-right': [0,0],
+      'bottom': [0, -markerHeight],
+      'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+      'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+      'left': [markerRadius, (markerHeight - markerRadius) * -1],
+      'right': [-markerRadius, (markerHeight - markerRadius) * -1]
+      };
+  var marker = new mapboxgl.Marker()
+  .setLngLat([restData.results[0].geometry.location.lng, restData.results[0].geometry.location.lat])
+  .addTo(map);
+ 
+    var popup = new mapboxgl.Popup({offset: popupOffsets, className: 'my-class'})
+    .setLngLat([restData.results[0].geometry.location.lng, restData.results[0].geometry.location.lat])
+    .setHTML(restData.results[0].name)
+    .setMaxWidth("300px")
+    .addTo(map);
+    var marker2 = new mapboxgl.Marker()
+    .setLngLat([restData.results[1].geometry.location.lng, restData.results[1].geometry.location.lat])
+    .addTo(map);
+   
+      var popup2 = new mapboxgl.Popup({offset: popupOffsets, className: 'my-class'})
+      .setLngLat([restData.results[1].geometry.location.lng, restData.results[1].geometry.location.lat])
+      .setHTML(restData.results[1].name)
+      .setMaxWidth("300px")
+      .addTo(map);
+      var marker3 = new mapboxgl.Marker()
+      .setLngLat([restData.results[2].geometry.location.lng, restData.results[2].geometry.location.lat])
+      .addTo(map);
+     
+        var popup3 = new mapboxgl.Popup({offset: popupOffsets, className: 'my-class'})
+        .setLngLat([restData.results[2].geometry.location.lng, restData.results[2].geometry.location.lat])
+        .setHTML(restData.results[2].name)
+        .setMaxWidth("300px")
+        .addTo(map);
+        var marker4 = new mapboxgl.Marker()
+      .setLngLat([restData.results[3].geometry.location.lng, restData.results[3].geometry.location.lat])
+      .addTo(map);
+     
+        var popup4 = new mapboxgl.Popup({offset: popupOffsets, className: 'my-class'})
+        .setLngLat([restData.results[3].geometry.location.lng, restData.results[3].geometry.location.lat])
+        .setHTML(restData.results[3].name)
+        .setMaxWidth("300px")
+        .addTo(map);
+        var marker5 = new mapboxgl.Marker()
+      .setLngLat([restData.results[4].geometry.location.lng, restData.results[4].geometry.location.lat])
+      .addTo(map);
+     
+        var popup5 = new mapboxgl.Popup({offset: popupOffsets, className: 'my-class'})
+        .setLngLat([restData.results[4].geometry.location.lng, restData.results[4].geometry.location.lat])
+        .setHTML(restData.results[4].name)
+        .setMaxWidth("300px")
+        .addTo(map);
+  }*/
+})
+const update =() =>
+{
+    if(typeof restData.results !="undefined")
+    {
+        setData(restData.results);
+    }
+    else
+        alert("Invalid Parameters");
+}
+const sortByDist = (e) =>
+{   
+  //  e.preventDefault();
+  //  let dataSet = data;
+   // alert(dataSet.results[0].name);
+  var dataSet = data;
+  if(typeof  dataSet != "undefined")
+  dataSet=  dataSet.sort(function(a, b) {
+        return b.geometry.location.lat - a.geometry.location.lat;
+      });
+      setData(dataSet);
+     // alert(dataSet[0].name)
+   //  n=0;
+
+ //  setData(dataSet);\
+
+    
+}
+const sortByRating = (e) =>
+{   
+  //  e.preventDefault();
+  //  let dataSet = data;
+   // alert(dataSet.results[0].name);
+  var dataSet = data;
+  if(typeof  dataSet != "undefined")
+  dataSet=  dataSet.sort(function(a, b) {
+        return b.rating - a.rating;
+      });
+      setData(dataSet);
+     // alert(dataSet[0].name)
+   //  n=0;
+
+ //  setData(dataSet);\
+
+    
+}
+const sortByPrice = (e) =>
+{
+   // e.preventDefault();
+    var dataSet = data;
+    if(typeof dataSet != "undefined")
+    dataSet=  dataSet.sort(function(a, b) {
+        return b.price_level - a.price_level;
+      }); 
+     setData(dataSet);
+    //  alert(dataSet[0].name)
+ //  n=0;
+}
+const sortByName = (e) =>
+{
+    var dataSet = data;
+   // e.preventDefault();
+   // var dataSet = data;
+    //if(typeof  data.results != "undefined")
+    if(typeof dataSet != "undefined")
+    dataSet= dataSet.sort(function(a, b) {
+        if(a.name<b.name)
+            return -1;
+        if(a.name > b.name )
+            return 1;
+        else
+            return 0;
+      }); 
+     setData(dataSet);
+      //n=0;
+}
+//{readData(restData)}
+return (
+<div>
 
 
-const rest = ({restData}) => (
-    <p>{restData}</p>
+
+  <Button variant="contained" color="secondary" onClick ={()=>update()}>Update Results</Button>
+  <Button variant="contained" color="secondary" onClick = {()=>sortByRating()}>Sort By Rating</Button>
+  <Button variant="contained" color="secondary"onClick = {()=>sortByPrice()}>Sort By Price</Button>
+  <Button variant="contained" color="secondary" onClick = {()=>sortByName()}>Sort By Name</Button>
+  <Button variant="contained" color="secondary" onClick = {()=>sortByDist()}>Sort By Distance</Button>
+  {( typeof data[0]!="undefined"&& data[0].business_status ==="OPERATIONAL") ? (
+        <Card  >
+          <CardContent>
+        <Typography  color="textSecondary" gutterBottom>
+        Restaurants: { data[0].name}
+        </Typography>
+        <Typography  color="textSecondary" gutterBottom>
+        Price: { data[0].price_level}
+        </Typography>
+        <Typography  color="textSecondary" gutterBottom>
+        Rating: { data[0].rating}
+        </Typography></CardContent>
+        <CardActions>
+        <Link href={`http://www.google.com/maps/place/${data[2].geometry.location.lat},${data[2].geometry.location.lng}`}>
+        Get Directions
+      </Link>
+        </CardActions>
+        </Card >
+        
+    ): (
+      <div></div>
+    )}
+   {( typeof data[1]!="undefined" && data[1].business_status ==="OPERATIONAL") ? (
+       <Card  >
+       <CardContent>
+     <Typography  color="textSecondary" gutterBottom>
+     Restaurants: { data[1].name}
+     </Typography><Typography  color="textSecondary" gutterBottom>
+        Price: { data[1].price_level}
+        </Typography>
+        <Typography  color="textSecondary" gutterBottom>
+        Rating: { data[1].rating}
+        </Typography></CardContent>
+        <CardActions>
+        
+        <Link href={`http://www.google.com/maps/place/${data[1].geometry.location.lat},${data[1].geometry.location.lng}`}>
+        Get Directions
+      </Link>
+        </CardActions>
+     </Card >
+    ): (
+      <div></div>
+    )}
+    
+    {(  typeof data[2]!="undefined" && data[2].business_status ==="OPERATIONAL") ? (
+       <Card  >
+       <CardContent>
+     <Typography  color="textSecondary" gutterBottom>
+     Restaurants: { data[2].name}
+     </Typography><Typography  color="textSecondary" gutterBottom>
+        Price: { data[2].price_level}
+        </Typography>
+        <Typography  color="textSecondary" gutterBottom>
+        Rating: { data[2].rating}
+        </Typography></CardContent>
+        <CardActions>
+        <Link href={`http://www.google.com/maps/place/${data[2].geometry.location.lat},${data[2].geometry.location.lng}`}>
+        Get Directions
+      </Link>
+        </CardActions>
+     </Card >
+    ): (
+      <div>
+        
+      </div>
+    )}
+    {( typeof data[3]!="undefined" && data[3].business_status ==="OPERATIONAL") ? (
+        
+        <Card  >
+          <CardContent>
+        <Typography  color="textSecondary" gutterBottom>
+        Restaurants: { data[3].name}
+        </Typography>
+        <Typography  color="textSecondary" gutterBottom>
+        Price: { data[3].price_level}
+        </Typography>
+        <Typography  color="textSecondary" gutterBottom>
+        Rating: { data[3].rating}
+        </Typography></CardContent>
+        <CardActions>
+        <Link href={`http://www.google.com/maps/place/${data[3].geometry.location.lat},${data[3].geometry.location.lng}`}>
+        Get Directions
+      </Link>
+        </CardActions>
+        </Card >
+    ): (
+      <div></div>
+    )}
+    {( typeof data[4]!="undefined" && data[4].business_status ==="OPERATIONAL") ? (
+        
+        <Card  >
+          <CardContent>
+        <Typography  color="textSecondary" gutterBottom>
+        Restaurants: { data[4].name}
+        </Typography><Typography  color="textSecondary" gutterBottom>
+        Price: { data[4].price_level}
+        </Typography>
+        <Typography  color="textSecondary" gutterBottom>
+        Rating: { data[4].rating}
+        </Typography></CardContent>
+        <CardActions>
+        <Link href={`http://www.google.com/maps/place/${data[4].geometry.location.lat},${data[4].geometry.location.lng}`}>
+        Get Directions
+      </Link>
+        </CardActions>
+        </Card >
+    ): (
+      <div></div>
+    )}
+    {( typeof data[5]!="undefined" && data[5].business_status ==="OPERATIONAL") ? (
+        
+        <Card  >
+        <CardContent>
+      <Typography  color="textSecondary" gutterBottom>
+      Restaurants: { data[5].name}
+      </Typography><Typography  color="textSecondary" gutterBottom>
+        Price: { data[5].price_level}
+        </Typography>
+        <Typography  color="textSecondary" gutterBottom>
+        Rating: { data[5].rating}
+        </Typography></CardContent>
+        <CardActions>
+        <Link href={`http://www.google.com/maps/place/${data[5].geometry.location.lat},${data[5].geometry.location.lng}`}>
+        Get Directions
+      </Link>
+        </CardActions>
+      </Card >
+    ): (
+      <div></div>
+    )}
+    {( typeof data[6]!="undefined" && data[6].business_status ==="OPERATIONAL") ? (
+        
+        <Card  >
+        <CardContent>
+      <Typography color="textSecondary" gutterBottom>
+      Restaurants: { data[6].name}
+      </Typography><Typography  color="textSecondary" gutterBottom>
+        Price: { data[6].price_level}
+        </Typography>
+        <Typography  color="textSecondary" gutterBottom>
+        Rating: { data[6].rating}
+        </Typography></CardContent>
+        <CardActions>
+        <Link href={`http://www.google.com/maps/place/${data[6].geometry.location.lat},${data[6].geometry.location.lng}`}>
+        Get Directions
+      </Link>
+        </CardActions>
+      </Card >
+    ): (
+      <div></div>
+    )}
+    {( typeof data[7]!="undefined" && data[7].business_status ==="OPERATIONAL") ? (
+        
+        <Card  >
+        <CardContent>
+      <Typography color="textSecondary" gutterBottom>
+      Restaurants: { data[7].name}
+      </Typography><Typography  color="textSecondary" gutterBottom>
+        Price: { data[7].price_level}
+        </Typography>
+        <Typography  color="textSecondary" gutterBottom>
+        Rating: { data[7].rating}
+        </Typography></CardContent>
+        <CardActions>
+        <Link href={`http://www.google.com/maps/place/${data[7].geometry.location.lat},${data[7].geometry.location.lng}`}>
+        Get Directions
+      </Link>
+        </CardActions>
+      </Card >
+    ): (
+      <div></div>
+    )}
+    {(  typeof data[8]!="undefined" &&data[8].business_status ==="OPERATIONAL") ? (
+        
+        <Card  >
+          <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+        Restaurants: { data[8].name}
+        </Typography><Typography  color="textSecondary" gutterBottom>
+        Price: { data[8].price_level}
+        </Typography>
+        <Typography  color="textSecondary" gutterBottom>
+        Rating: { data[8].rating}
+        </Typography></CardContent>
+        <CardActions>
+        <Link href={`http://www.google.com/maps/place/${data[8].geometry.location.lat},${data[8].geometry.location.lng}`}>
+        Get Directions
+      </Link>
+        </CardActions>
+        </Card >
+    ): (
+      <div></div>
+    )} <div ref={el => (mapContainer.current = el)}style={styles} />
+
+</div>
 )
+}
 
-export default rest;
+export default Rest;
